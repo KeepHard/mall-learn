ad5d92f56aa519b67faabd8c04e5dcd6eaa4f019
AIHS <1245618964@qq.com>
2023-03-09 16:53:32 +0800

    mall-tiny-01 mybatis generator 生成model、mapper

--- mall-tiny-01/src/main/java/com/aihs/mall/tiny/mgb/Generator.java
+++ mall-tiny-01/src/main/java/com/aihs/mall/tiny/mgb/Generator.java
@@ -15,6 +15,7 @@ public class Generator {
         InputStream is = Generator.class.getResourceAsStream("/generatorConfig.xml");
         ConfigurationParser cp = new ConfigurationParser(warnings);
         Configuration config = cp.parseConfiguration(is);
+        is.close();
         DefaultShellCallback callback = new DefaultShellCallback(overwrite);
         MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
         myBatisGenerator.generate(null);
