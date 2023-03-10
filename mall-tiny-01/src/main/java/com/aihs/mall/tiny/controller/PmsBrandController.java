ab7c1aa06f9c3f00e1cf5dafbbe2baf7551d2c1b
AIHS <1245618964@qq.com>
2023-03-10 17:13:27 +0800

    mall-tiny-01 springBoot与MyBatis整合搭建完成

--- mall-tiny-01/src/main/java/com/aihs/mall/tiny/controller/PmsBrandController.java
+++ mall-tiny-01/src/main/java/com/aihs/mall/tiny/controller/PmsBrandController.java
@@ -4,6 +4,8 @@ import com.aihs.mall.tiny.common.api.CommonPage;
 import com.aihs.mall.tiny.common.api.CommonResult;
 import com.aihs.mall.tiny.mgb.model.PmsBrand;
 import com.aihs.mall.tiny.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/brand")
+@Api(tags = "PmsBrandController",description = "商品品牌管理")
 public class PmsBrandController {
 
 
@@ -29,12 +32,14 @@ public class PmsBrandController {
 
     @ResponseBody
     @RequestMapping(value = "/listAll",method = RequestMethod.GET)
+    @ApiOperation("获取所有品牌列表")
     public CommonResult<List<PmsBrand>> getBrandList(){
         return CommonResult.success(brandService.listAllBrand());
     }
 
     @ResponseBody
     @RequestMapping(value = "/list",method = RequestMethod.GET)
+    @ApiOperation("分页查询品牌列表")
     public CommonResult<CommonPage<PmsBrand>> listBrand(@RequestParam(value = "pageNum",defaultValue = "3") Integer pageNum,
                                                         @RequestParam(value = "pageSize",defaultValue = "3") Integer pageSize){
         List<PmsBrand> brandList = brandService.listBrand(pageNum,pageSize);
@@ -43,6 +48,7 @@ public class PmsBrandController {
 
     @ResponseBody
     @RequestMapping(value = "/create",method = RequestMethod.POST)
+    @ApiOperation("创建品牌")
     public CommonResult createBrand(@RequestBody PmsBrand brand){
         CommonResult commonResult;
         int count = brandService.createBrand(brand);
@@ -58,6 +64,7 @@ public class PmsBrandController {
 
     @ResponseBody
     @RequestMapping(value = "/update/{id}",method = RequestMethod.POST)
+    @ApiOperation("更新品牌")
     public CommonResult updateBrand(@PathVariable("id") Long id,@RequestBody PmsBrand brand){
         CommonResult commonResult;
         int count = brandService.updateBrand(id, brand);
@@ -73,6 +80,7 @@ public class PmsBrandController {
 
     @ResponseBody
     @RequestMapping(value = "/delete/{id}",method = RequestMethod.DELETE)
+    @ApiOperation("删除品牌")
     public CommonResult deleteBrand(@PathVariable("id") Long id){
         CommonResult commonResult;
         int count = brandService.deleteBrand(id);
--- mall-tiny-01/src/main/java/com/aihs/mall/tiny/mgb/CommentGenerator.java
+++ mall-tiny-01/src/main/java/com/aihs/mall/tiny/mgb/CommentGenerator.java
@@ -1,12 +1,15 @@
 package com.aihs.mall.tiny.mgb;
 
+import io.swagger.annotations.ApiModelProperty;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
 import org.mybatis.generator.internal.util.StringUtility;
 
 import java.util.Properties;
+import java.util.Set;
 
 public class CommentGenerator extends DefaultCommentGenerator {
     private boolean addRemarkComments = false;
@@ -15,6 +18,16 @@ public class CommentGenerator extends DefaultCommentGenerator {
     public void addConfigurationProperties(Properties properties){
         super.addConfigurationProperties(properties);
         this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
+    }
+
+    @Override
+    public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> imports) {
+        super.addFieldAnnotation(field, introspectedTable, imports);
+        String remarks = introspectedTable.getRemarks();
+        imports.add(new FullyQualifiedJavaType("io.swagger.annotations.ApiModelProperty"));
+        if(addRemarkComments&&StringUtility.stringHasValue(remarks)){
+
+        }
     }
 
     @Override
